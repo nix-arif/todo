dc7177820017ff3b3c7dad7d341e216d13b4a5b9
nix-arif <nix.arif@gmail.com>
2019-11-12 09:24:10 +0800

    Complete todolistapp tutorial

--- posts/models.py
+++ posts/models.py
@@ -5,3 +5,6 @@ class Todo(models.Model):
     added_date = models.DateTimeField()
     text = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.text
+
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Todo
 
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'posts/index.html')
+    todo_items = Todo.objects.all().order_by('-added_date')
+    return render(request, 'posts/index.html', {'todo_items': todo_items})
 
+@csrf_exempt
 def add_todo(request):
+    added_date = timezone.now()
+    content = request.POST['content']
+    Todo.objects.create(added_date=added_date, text=content)
+    return HttpResponseRedirect("/")
+
+@csrf_exempt
+def delete_todo(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
     return HttpResponseRedirect("/")
